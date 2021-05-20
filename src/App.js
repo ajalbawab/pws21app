e2c879f8ed80255432a324a2e0b4b53516a58efe
AJ <ajalbawab@gmail.com>
2021-05-20 11:36:20 -0400

    on to working on full website

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload Ross.
+          Working on website.. come back later.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.linkedin.com/in/ajalbawab/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Click Me
         </a>
       </header>
     </div>
