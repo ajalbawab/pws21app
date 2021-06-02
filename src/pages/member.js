fd5ecf7962d18df8ba818f2c157b4708d40279c2
ajalbawab <68740075+ajalbawab@users.noreply.github.com>
2021-06-01 20:25:32 -0400

    added Pranshu and LCave

--- src/pages/member.js
+++ src/pages/member.js
@@ -43,12 +43,14 @@ class Member extends React.Component {
                <div className={styles.modal_firstlightdetails}>
                  {cohort[member].gpa && <div style={{display:'flex'}}><div style={{fontWeight:'bold',marginRight:10}}>GPA:</div>{cohort[member].gpa}</div>}
                  {cohort[member].classstanding && <div style={{display:'flex'}}><div style={{fontWeight:'bold',marginRight:10}}>Class Designation:</div> {cohort[member].classstanding}</div>}
-                 {cohort[member].interests && <div style={{display:'flex'}}><div style={{fontWeight:'bold',marginRight:10}}>Interest:</div> {cohort[member].interests}</div>}
+                 {cohort[member].interests && <div style={{display:'flex'}}><div style={{fontWeight:'bold',marginRight:10}}>Interests:</div> {cohort[member].interests}</div>}
                </div>
                {cohort[member].gpa && cohort[member].classstanding && cohort[member].interests && <div style={{height:2, backgroundColor:'black', marginTop:10, marginBottom:10}}> </div>}
                <div className={styles.modal_deepdetails}>
-               {cohort[member].bio && <div style={{display:'flex', marginBottom:10}}><div style={{fontWeight:'bold',marginRight:10}}>Biography:</div>{cohort[member].bio}</div>}
-                 {cohort[member].accomplishments && <div style={{display:'flex'}}><div style={{fontWeight:'bold',marginRight:10}}>Accomplishments:</div> {cohort[member].accomplishments}</div>}
+               {cohort[member].bio && <div style={{display:'flex', marginBottom:5}}><div style={{fontWeight:'bold',marginRight:10}}>Biography:</div>{cohort[member].bio}</div>}
+                 {cohort[member].accomplishments && <div style={{display:'flex', marginBottom:5}}><div style={{fontWeight:'bold',marginRight:10}}>Accomplishments:</div> {cohort[member].accomplishments}</div>}
+                 {cohort[member].scholarships && <div style={{display:'flex', marginBottom:5}}><div style={{fontWeight:'bold',marginRight:10}}>Scholarships:</div> {cohort[member].scholarships}</div>}
+                 {cohort[member].clubs && <div style={{display:'flex', marginBottom:5}}><div style={{fontWeight:'bold',marginRight:10}}>Clubs:</div> {cohort[member].clubs}</div>}
                  {cohort[member].resume && <div style={{display:'flex'}}><div style={{fontWeight:'bold',marginRight:10}}>Resume:   </div><a href={cohort[member].resume}>Click to view {cohort[member].name }'s' resume</a></div>}
                  
                </div>
